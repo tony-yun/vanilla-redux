3ab110177446b11e8e036a3f8e90315d5e0c3f30
윤태웅 <yuntaewoong@yuntaeung-ui-MacBookPro.local>
2022-10-08 23:25:39 +0900

    1008 redux study

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
+import { legacy_createStore as createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-const updateText = () => {
-  number.innerText = count;
+//state가 없으면 default로 0으로 호출한다.
+const reducer = (state = 0, action) => {
+  console.log(state, action);
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  } else {
+    return state;
+  }
 };
+
+const store = createStore(reducer);
+
+const onChange = () => {
+  number.innerText = store.getState();
+};
+
+store.subscribe(onChange);
+
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  store.dispatch({ type: "ADD" });
 };
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  store.dispatch({ type: "MINUS" });
 };
 
 add.addEventListener("click", handleAdd);
